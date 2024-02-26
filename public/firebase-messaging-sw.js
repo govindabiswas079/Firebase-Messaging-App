1d192ce5c548e4722372509910055f476befb704
Govinda Biswas <75020328+govindabiswas079@users.noreply.github.com>
2024-02-26 10:23:24 +0530

    Update config firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -78,3 +78,41 @@ messaging.onBackgroundMessage(function (payload) {
  
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
+
+
+// self.addEventListener('push', function (event) {
+//     console.log('[Service Worker] Push Received.');
+//     const payload = event.data ? event.data.json() : {};
+//     console.log('[Service Worker] Push Payload:', payload);
+
+//     // Extract necessary data from the payload
+//     const title = payload.notification.title || 'Default Title';
+//     const options = {
+//         body: payload.notification.body || 'Default Body',
+//         icon: payload.notification.icon || 'icon.png'
+//     };
+
+//     // Directly open the desired URL when notification is clicked
+//     self.addEventListener('notificationclick', function (event) {
+//         console.log('Notification clicked!');
+//         event.notification.close();
+
+//         const urlToOpen = 'http://localhost:3000/admin/account/profile?_username=govindbiswas079&_user=65a815d751b20870641b132b';
+
+//         event.waitUntil(
+//             clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
+//                 for (let i = 0; i < windowClients.length; i++) {
+//                     const client = windowClients[i];
+//                     if (client.focused) {
+//                         return client.navigate(urlToOpen);
+//                     }
+//                 }
+//                 if (clients.openWindow) {
+//                     return clients.openWindow(urlToOpen);
+//                 }
+//             })
+//         );
+//     });
+// });
+
